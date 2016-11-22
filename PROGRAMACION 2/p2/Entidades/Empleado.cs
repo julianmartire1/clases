6e3d15cc27a8ba3d7e962dafa1a9fb0b8a0d8c06
julianmartire1 <julianmartire.jm@gmail.com>
2016-11-22 19:17:05 -0300

    p2

---
+++ PROGRAMACION 2/p2/Entidades/Empleado.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Entidades
+{
+    [Serializable]
+    public class Empleado : Persona, IArchivos<Empleado>
+    {
+
+        private int _legajo;
+
+        public override int Legajo
+        {
+            get
+            {
+                return this._legajo;
+            }
+            set
+            {
+                this._legajo = value;
+            }
+        }
+
+        public Empleado(string nombre, string apellido, int legajo)
+            : base(nombre, apellido)
+        {
+            this.Legajo = legajo;
+        }
+
+        public Empleado()
+        {
+            // Para poder serializar.
+        }
+
+        public static string MostrarDatos(Empleado e)
+        {
+            return e.ToString();
+        }
+
+        public bool guardar(string archivo, Empleado datos)
+        {
+            XmlTextWriter writer;
+            XmlSerializer ser;
+
+            try
+            {
+                writer = new XmlTextWriter(archivo, Encoding.UTF8);
+                ser = new XmlSerializer(typeof(Empleado));
+
+                ser.Serialize(writer, datos);
+
+                writer.Close();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw new NoGuardoException(e);
+            }
+        }
+
+        public bool leer(string archivo, out Empleado datos)
+        {
+            XmlTextReader reader;
+            XmlSerializer ser;
+
+            try
+            {
+                reader = new XmlTextReader(archivo);
+                ser = new XmlSerializer(typeof(Empleado));
+
+                datos = (Empleado)ser.Deserialize(reader);
+
+               // reader.Close();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                datos = default(Empleado);
+                throw new NoLeeException(e);
+            }
+        }
+    }
+}
---
+++ PROGRAMACION 2/p2/Esqueleto2doParcial-20161114/frmParcial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Entidades;
+
+namespace Esqueleto2doParcial_20161114
+{
+    public partial class frmParcial : Form
+    {
+        private Empleado _emp;
+        private string path;
+
+        public frmParcial()
+        {
+            InitializeComponent();
+            this._emp = new Empleado("Julian", "Martire", 104303);
+            this.path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\empleado.xml";
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Thread hilo = new Thread(this.guardar);
+            hilo.Start();
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            Thread hilo = new Thread(this.leer);
+            hilo.Start();
+        }
+
+        public void guardar()
+        {
+            this._emp.guardar(this.path, this._emp);
+        }
+        public void leer()
+        {
+            Empleado aux;
+
+            if (this._emp.leer(this.path, out aux))
+            {
+                MessageBox.Show(aux.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Empleado no leído");
+            }
+        }
+    }
+}
